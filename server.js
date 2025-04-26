b238404426e5dba9738b0e25301aa4457eeb8b9a
miretis <vo3807473@gmail.com>
2025-04-26 15:33:06 +0300

    Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -15,18 +15,33 @@ const server = http.createServer((req, res)=>{
 
 
     switch(req.url){
-        case "/":
+        case "/index":
             return res.end(indexHtmlFile);
         case "/style.css":
             return res.end(fileCSS);
         case "/script.js":
             return res.end(scriptJS);
+        case "/add":
+            addMessage(res, req);
+        case "/bobr":
+            return res.end(`<html><body><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/640px-American_Beaver.jpg"></body></html>`);
+            break;
         default:
             res.statusCode = 404;
             return res.end('Error 404');
     }
-        
-
 });
+
+function addMessage(res,req){
+    let data = ""
+    req.on('data', function(chunk){
+        data += chunk;
+    })
+    req.on('end', function(){
+        console.log(data);
+    })
+    res.writeHead(302, {Location: '/'});
+    res.end();
+}
 
 server.listen(3000);
