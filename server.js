1fb8ca7be03d95c33ce99f9c2080ef89b3aa50ca
miretis <vo3807473@gmail.com>
2025-04-26 14:30:40 +0300

    Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ const path = require('path');
 const  pathToIndex =  path.join(__dirname, 'static', 'index.html');
 const indexHtmlFile = fs.readFileSync(pathToIndex);
 
+const pathToCss  =  path.join(__dirname, 'static', 'style.css');
+const fileCSS = fs.readFileSync(pathToCss);
+
+const pathToJS  =  path.join(__dirname, 'static', 'script.js');
+const scriptJS = fs.readFileSync(pathToJS);
+
 const server = http.createServer((req, res)=>{
 
 
-    if(req.url === '/'){
-        return res.end(indexHtmlFile);
+    switch(req.url){
+        case "/":
+            return res.end(indexHtmlFile);
+        case "/style.css":
+            return res.end(fileCSS);
+        case "/script.js":
+            return res.end(scriptJS);
+        default:
+            res.statusCode = 404;
+            return res.end('Error 404');
     }
-    res.statusCode = 404;
-    return res.end('Error 404');
+        
+
 });
 
 server.listen(3000);
