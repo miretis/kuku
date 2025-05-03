cfcf0da68a632d8b1256412411618ef3dc1881f3
miretis <vo3807473@gmail.com>
2025-05-03 14:31:05 +0300

    sdfbn

--- server.js
+++ server.js
@@ -2,46 +2,57 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const  pathToIndex =  path.join(__dirname, 'static', 'index.html');
+const pathToIndex = path.join(__dirname, 'static', 'index.html');
 const indexHtmlFile = fs.readFileSync(pathToIndex);
 
-const pathToCss  =  path.join(__dirname, 'static', 'style.css');
-const fileCSS = fs.readFileSync(pathToCss);
+const pathToJS= path.join(__dirname, 'static', 'script.js');
+const JSFile = fs.readFileSync(pathToJS);
 
-const pathToJS  =  path.join(__dirname, 'static', 'script.js');
-const scriptJS = fs.readFileSync(pathToJS);
+
+const pathToCSS= path.join(__dirname, 'static', 'style.css');
+const CSSFile = fs.readFileSync(pathToCSS);
 
 const server = http.createServer((req, res)=>{
-
-
+    //Liza ostrologiya eto ne nayka
     switch(req.url){
-        case "/index":
-            return res.end(indexHtmlFile);
-        case "/style.css":
-            return res.end(fileCSS);
+        case "/":
+            return res.end(indexHtmlFile); 
         case "/script.js":
-            return res.end(scriptJS);
+            return res.end(JSFile); 
+        case "/style.css":
+            return res.end(CSSFile);
         case "/add":
             addMessage(res, req);
-        case "/bobr":
-            return res.end(`<html><body><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/640px-American_Beaver.jpg"></body></html>`);
             break;
         default:
             res.statusCode = 404;
             return res.end('Error 404');
     }
 });
-
-function addMessage(res,req){
+server.listen(3000);
+function addMessage(res, req){
     let data = ""
     req.on('data', function(chunk){
         data += chunk;
     })
-    req.on('end', function(){
+    req.on('end',function(){
         console.log(data);
-    })
+    })    
     res.writeHead(302, {Location: '/'});
     res.end();
 }
 
-server.listen(3000);
+
+
+
+
+
+// 🧠 Задание 3: Добавь счётчик сообщени
+// Что нужно сделать:
+// В файле сервера (server.js) добавь переменную, которая будет считать количество отправленных сообщений.
+
+// Каждый раз при получении нового сообщения увеличивай счётчик на 1.
+
+// Передавай значение счётчика на главную страницу и отображай его в тег messages count.
+
+// Результат: На главной странице отображается количество отправленных сообщений.
